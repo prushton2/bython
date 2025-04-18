7da789298f3a2f1662daa61355de1bf1c60e33d6
prushton2 <pterrushton418@gmail.com>
2025-04-18 12:45:24 -0400

    restructured parser, added fstring test, fixed map detection

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,6 @@
 import re
 import os
-from tokenize import tokenize, tok_name, INDENT, DEDENT, NAME
+from tokenize import tokenize, tok_name, INDENT, DEDENT, NAME, TokenInfo
 from tokenize import open as topen;
 """
 Python module for converting bython code to python code.
@@ -106,72 +106,191 @@ def parse_file(infilepath, outfilepath, add_true_line,  utputname=None, change_i
     
     tokenfile = open(infilepath, 'rb')
     tokens = list(tokenize(tokenfile.readline))
+
     tokens.pop(0)
-     
-     # some control variables to make sure we dont parse content in fstrings and dict definitions
-     
-    inside_fstring = False
-    inside_dict = False
+
+    newTokens = parse_indentation(tokens)
     
-    for i, j in enumerate(tokens):
+    newTokens = parse_and_or(newTokens)
+
+    # some control variables to make sure we dont parse content in fstrings and dict definitions
+     
+    # inside_fstring = False
+    # dict_layer = 0
+    
+    # for i, j in enumerate(tokens):
         #print(j) #61 and 63
         #write line with indentation
         
-        if(j.type == 61):
-            inside_fstring = True
-        elif(j.type == 63):
-            inside_fstring = False
+        # if(j.type == 61):
+        #     inside_fstring = True
+        # elif(j.type == 63):
+        #     inside_fstring = False
         
-        if(j.string == '=' and tokens[i+1].string == '{'):
-            inside_dict = True
-        if(inside_dict and tokens[i-1].string == '}'): # We check if the previous token was the end, and then we can continue parsing
-            inside_dict = False
-         
-         
-         
-        if(inside_fstring or inside_dict): # No processing should be done inside an fstring
-            current_line += j.string
-            continue;
+        # if(i >= 2 and tokens[i-2].type == 1 and tokens[i-1].string == '=' and j.string == '{'):
+        #     dict_layer = 1
+        #     # current_line += j.string
+
+        # elif(dict_layer >= 1 and tokens[i].string == '{'):
+        #     dict_layer += 1
+        #     # current_line += j.string
+
+
+        # elif(dict_layer >= 1 and tokens[i].string == '}'):
+        #     dict_layer -= 1
+        #     if(dict_layer == 0):
+        #         current_line += j.string
+
+        
+        # if(inside_fstring or dict_layer >= 1): # No processing should be done inside an fstring
+        #     # print(j.string)
+        #     current_line += j.string
+        #     continue
     
-        if j.string == "{":
-            indentation_level += 1
-            current_line += ":"
-        elif j.string == "}":
-            indentation_level -= 1
-            outfile.write(current_line + "\n");
-            current_line = indentation_level * indentation_sign
+        # if j.string == "{":
+        #     indentation_level += 1
+        #     current_line += ":"
+        # elif j.string == "}":
+        #     indentation_level -= 1
+        #     outfile.write(current_line + "\n");
+        #     current_line = indentation_level * indentation_sign
     
         #check for && and replace with and
-        elif j.string == "&" and tokens[i+1].string == "&":
-            current_line += " and "
+        # elif j.string == "&" and tokens[i+1].string == "&":
+        #     current_line += " and "
         
-        elif j.string == "&" and tokens[i-1].string == "&":
-            continue
+        # elif j.string == "&" and tokens[i-1].string == "&":
+        #     continue
     
-        #check for || and replace with or
-        elif j.string == "|" and tokens[i+1].string == "|":
-            current_line += " or "
-        elif j.string == "|" and tokens[i-1].string == "|":
-            continue
+        # #check for || and replace with or
+        # elif j.string == "|" and tokens[i+1].string == "|":
+        #     current_line += " or "
+        # elif j.string == "|" and tokens[i-1].string == "|":
+        #     continue
     
-        else:
-            current_line += j.string
+        # else:
+        #     current_line += j.string
     
     
         #adds a space after NAME tokens, so def main doesnt become defmain
-        if j.type == 1:
-            current_line += " "
-        #on a newline, write the current line and restart
-        if j.string == "\n":
-            outfile.write(current_line)
-            current_line = indentation_level * indentation_sign
-    
+        # if j.type == 1:
+        #     current_line += " "
+        # #on a newline, write the current line and restart
+        # if j.string == "\n":
+        #     outfile.write(current_line)
+        #     current_line = indentation_level * indentation_sign
+    for(i, j) in enumerate(newTokens):
+        # print(j)
+        outfile.write(j.string)
+        if(j.type == 1):
+            outfile.write(" ")
+
     infile.close()
     outfile.close()
-    # Add 'pass' where there is only a {}. 
-    # 
-    # DEPRECATED FOR NOW. This way of doing
-    # it is causing a lot of problems with {} in comments. The feature is removed
-    # until I find another way to do it. 
+
+def gen_indent(indentationLevel):
+    arr = []
+    for i in range(indentationLevel):
+        arr.append(
+            TokenInfo(
+                type=5,
+                string='    ',
+                start=(),
+                end=(),
+                line=""
+            )
+        )
     
-    # infile_str_raw = re.sub(r"{[\s\n\r]*}", "{\npass\n}", infile_str_raw)
+    return arr
+
+def parse_indentation(tokens):
+    newTokens = []
+    indentationLevel = 0
+    mapdepth = 0
+
+    for i, j in enumerate(tokens):
+        
+        # We find the start of a map. We need to set depth to 1, add the { token, and done
+        if( i >= 2 and tokens[i-2].type == 1 and tokens[i-1].string == "=" and j.string == "{"):
+            mapdepth = 1
+            newTokens.append(j)
+            continue
+
+        # We're inside a map, so we add the token
+        if(mapdepth >= 1):
+            newTokens.append(j)
+
+        # We update how deep we are in the map. If this changes, we're done 
+        if( i >= 2 and mapdepth >= 1 and tokens[i].string == "{"):
+            mapdepth += 1
+            continue
+        if( i >= 2 and mapdepth >= 1 and tokens[i].string == "}"):
+            mapdepth -= 1
+            continue
+
+        # if we're inside a map, we've added the token and we have to ignore the curlies, so we're done
+        if(mapdepth != 0):
+            continue
+
+        if (j.string == "{"):
+            indentationLevel += 1
+            newTokens.append(
+                TokenInfo(
+                    type=55,
+                    string=":",
+                    start=j.start,
+                    end=j.end,
+                    line=j.line
+                )
+            )
+            newTokens.extend(gen_indent(indentationLevel))
+            continue
+
+        if(j.string == "}"):
+            indentationLevel -= 1
+            continue
+        
+
+        newTokens.append(j)
+
+        if(newTokens[-1].string == "\n"):
+            newTokens.extend(gen_indent(indentationLevel))
+    
+    return newTokens
+
+
+def parse_and_or(tokens):
+    # print(tokens)
+    newTokens = []
+
+    for i, j in enumerate(tokens):
+        if(j.string == "&" and tokens[i+1].string == "&"):
+            newTokens.append(
+                TokenInfo(
+                    type=1,
+                    string="and",
+                    start=(),
+                    end=(),
+                    line=""
+                )
+            )
+            continue
+        if(j.string == "&" and tokens[i-1].string == "&"):
+            continue
+        
+        if(j.string == "|" and tokens[i+1].string == "|"):
+            newTokens.append(
+                TokenInfo(
+                    type=1,
+                    string="or",
+                    start=(),
+                    end=(),
+                    line=""
+                )
+            )
+            continue
+        if(j.string == "|" and tokens[i-1].string == "|"):
+            continue
+
+        newTokens.append(j)
+    return newTokens
